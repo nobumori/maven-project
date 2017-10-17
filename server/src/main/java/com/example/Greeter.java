8f8f53c89019df545881228a487275fbbecba19a
nobumori <nobumori@mail.ru>
2017-10-17 13:40:19 +0300

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
 
   /**
-  *@param bla bla 
-  *@return bla
+  *@param someone
+  *@return greeting string
   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
