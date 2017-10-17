6afabe0cd20eb17f3765b0462873dd86c9fb6472
nobumori <nobumori@mail.ru>
2017-10-17 13:28:48 +0300

    bla bla

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,10 +12,6 @@ public class Greeter {
 
   }
 
-/**
-*@param somethimg change
-*@return greeting string
-*/
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
