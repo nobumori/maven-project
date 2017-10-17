500d68661b16f36021dc305b162129ce814e7704
nobumori <nobumori@mail.ru>
2017-10-17 13:36:59 +0300

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,6 +12,10 @@ public class Greeter {
 
   }
 
+  /**
+  *@param bla bla 
+  *@return bla
+  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
